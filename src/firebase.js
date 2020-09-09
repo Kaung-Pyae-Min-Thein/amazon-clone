ebb0d59585deeec29353a10988f5b860604f46ed
Kaung-Pyae-Min-Thein <kaungpyae.kg@gmail.com>
2020-09-09 21:28:52 +0630

    Very Start Stage

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA6QPkYjiELnK9l5RR9Il6tcGm75TgWEro",
+  authDomain: "amaozn-challenge.firebaseapp.com",
+  databaseURL: "https://amaozn-challenge.firebaseio.com",
+  projectId: "amaozn-challenge",
+  storageBucket: "amaozn-challenge.appspot.com",
+  messagingSenderId: "780865721779",
+  appId: "1:780865721779:web:71c1fc64ce3920caeb27c6",
+  measurementId: "G-7MM78RCZLV",
+};
